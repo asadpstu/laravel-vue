99ce511cdf978b6cc093ae4c96086a392551c402
Asad <hmasad09@gmail.com>
2018-11-09 21:41:57 +0800

    simple vue route with css

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//You user refresh page but content is from vue
+Route::get('{path}', "HomeController@index")->where('path','([A-z\d-\/_.]+)?');
